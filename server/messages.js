78491b164e8d5c7639e57ab374fe1d67d8c03433
tiagoomendess <tiago@mendes.com.pt>
2018-10-17 18:35:25 +0100

    a2

--- server/messages.js
+++ server/messages.js
@@ -3,27 +3,51 @@ const express = require('express');
 
 function MessegesRouter() {
 
-  let router = express();
+    let router = express();
 
-  router.use(bodyParser.json({ limit: '100mb' }));
-  router.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+    router.use(bodyParser.json({ limit: '100mb' }));
+    router.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
-  router.route('/received')
+    router.route('/received')
 
-    .get(function(req, res, next) {
-        console.log('GET /messages/received');
-        res.status(200);
-        res.send("Ok");
-    })
-    
-    .post(function (req, res, next) {
-        console.log('POST /messages/received');
-        res.status(200);
-        res.send("Ok");
-      next();
-    });
+        .get(function (req, res, next) {
+            console.log('GET /messages/received');
 
-  return router;
+            // Your verify token. Should be a random string.
+            let VERIFY_TOKEN = process.env.APP_ACCESS;
+
+            // Parse the query params
+            let mode = req.query['hub.mode'];
+            let token = req.query['hub.verify_token'];
+            let challenge = req.query['hub.challenge'];
+
+            // Checks if a token and mode is in the query string of the request
+            if (mode && token) {
+
+                // Checks the mode and token sent is correct
+                if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+                    // Responds with the challenge token from the request
+                    console.log('WEBHOOK_VERIFIED');
+                    res.status(200).send(challenge);
+
+                } else {
+                    // Responds with '403 Forbidden' if verify tokens do not match
+                    res.sendStatus(403);
+                }
+            }
+            res.status(200);
+            res.send("Ok");
+        })
+
+        .post(function (req, res, next) {
+            console.log('POST /messages/received');
+            res.status(200);
+            res.send("Ok");
+            next();
+        });
+
+    return router;
 }
 
 module.exports = MessegesRouter;
