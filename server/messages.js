9aa76db759d5d4e074d1f2a759dceae7fcfc2916
tiagoomendess <tiago@mendes.com.pt>
2018-10-17 20:11:08 +0100

    a10

--- server/messages.js
+++ server/messages.js
@@ -41,15 +41,17 @@ function MessegesRouter() {
 
         .post(function (req, res, next) {
             console.log('POST /messages/received');
-            console.log(req.body.entry.length);
+            //console.log(req.body.entry.length);
 
             for(var i = 0; i < req.body.entry.length; i++) {
                 
-                console.log(req.body.entry[i].messaging);
+                //console.log(req.body.entry[i].messaging);
                 //let sender_id = req.body.entry[i].messaging.sender.id;
                 //let timestamp = req.body.entry[i].messaging.timestamp;
                 //let message = req.body.entry[i].messaging.message.text;
 
+            
+                console.log(req.body.entry[i].messaging.message);
                 //console.log("[" + sender_id + "] @(" + timestamp + ") -> " + message);
             }
 
