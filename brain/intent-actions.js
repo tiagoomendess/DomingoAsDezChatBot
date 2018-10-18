d625252a5959a865cc233e9d0bc84dc0fc83c327
tiagoomendess <tiago@mendes.com.pt>
2018-10-18 22:36:46 +0100

    a

--- brain/intent-actions.js
+++ brain/intent-actions.js
@@ -10,16 +10,20 @@ function tellScoreLiveMatch(sender) {
 
     request(options, function (error, response, body) {
         
-        if (response.statusCode === 200 && body.data !== undefined) {
+        if (response.statusCode === 200) {
 
             if (body.data.length === 0) {
                 console.log("\t[BRAIN]: No live games, doing nothing!");
                 setTimeout(function() {
                     Bot.SendAction(sender.sender_id, 'mark_seen');
                 }, 5000);
+            } else {
+                console.log("\t[BRAIN]: There are games now!");
             }
 
         }
+
+        console.log("fim");
 
     });
 }
